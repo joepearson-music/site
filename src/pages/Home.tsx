3c42cf77bfbe0b1d7b4ce2c5aebadee4e2796f2f
jackforevergreen <jack@forevergreen.earth>
2024-09-21 18:54:22 -0400

    Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,25 +2,8 @@ import React from "react";
 
 const About: React.FC = () => (
   <div style={{ padding: "20px", maxWidth: "800px", margin: "0 auto" }}>
-    <h2>About Me</h2>
-    <p>
-      Hi! My name is Jack Pearson and I am in my last semester at the University
-      of Pittsburgh. I enjoy finance, business, software development, writing,
-      sustainability and learning. I love making websites so I figured I would
-      make a personal one to host projects and have a central location to show
-      my progress.
-    </p>
-    <p>
-      I am building a startup www.forevergreen.earth to fight climate change.
-    </p>
-    <h3>My Interests</h3>
-    <ul>
-      <li>Finance</li>
-      <li>Business</li>
-      <li>Software Development</li>
-      <li>Writing</li>
-      <li>Sustainability</li>
-    </ul>
+    <h2>Home</h2>
+    <p>Hi! Welcome to my personal website.</p>
   </div>
 );
 
